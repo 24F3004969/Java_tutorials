2400634475017f0b803480f6efe18f2b78a6087b
Hilal-Anwar <hilal123anwar@gmail.com>
2026-01-11 17:59:12 +0530

    rearrangement of code

--- src/main/java/org/java_tutorials/tuition/shahir/part_1/generics/Fun.java
+++ src/main/java/org/java_tutorials/tuition/shahir/part_1/generics/Fun.java
@@ -1,6 +1,6 @@
 package org.java_tutorials.tuition.shahir.part_1.generics;
 
-import org.java_tutorials.dsa.algorithem.Grid;
+import org.java_tutorials.dsa.algorithm.Grid;
 
 public class Fun <T extends Grid,U,V,W,X,Y>{
 }
